b8f31ee1b169294b79b2feb5d8cfe4e27ba7bf6a
LeoFu9487 <leofu890806@gmail.com>
2021-05-21 22:51:32 +0200

    todo : basic test, readme

---
+++ basic_test.sh
@@ -0,0 +1,58 @@
+ROOT=..
+TRACE=./trace_basic
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
+make -C $ROOT
+
+clear
+
+if [[ -f "$ROOT/push_swap" ]]
+then
+	printf "${GREEN}push_swap compilation ok${NOCOLOR}\n\n"
+else
+	printf "${RED}push_swap compilation ko${NOCOLOR}\n\n"
+	exit 1
+fi
+
+FLAG="${GREEN}OK${NOCOLOR}"
+LEAKFLAG="${GREEN}NO LEAKS${NOCOLOR}"
+
+printf "_____________ERROR_______________TEST_____________\n\n"
+
+for ((i=1;i<=15;i++));
+do
+	printf "test_case_$i\n"
+	$ROOT/push_swap $(cat $TRACE/error_files/test_case_$i.txt) >stdout 2>stderr
+	if [[ -s stdout ]];
+	then
+		printf "${RED}KO, stdout must be empty${NOCOLOR}\n"
+		FLAG="${RED}KO${NOCOLOR}"
+	else
+		printf "${GREEN}OK${NOCOLOR}\n"
+	fi
+	if [[ -s stderr ]];
+	then
+		printf "Error\n" > e
+		diff e stderr > x
+		if [[ -s x ]]
+		then
+			printf "${RED}KO, Output message in stderr is wrong${NOCOLOR}"
+		else
+			printf "${GREEN}OK${NOCOLOR}\n"
+		fi
+		rm -rf e x
+	else
+		printf "${RED}KO, \"Error\" must be written in stderr followed by a newline${NOCOLOR}\n"
+		FLAG="${RED}KO${NOCOLOR}"
+	fi
+	rm -rf stdout stderr
+done
+
+
+
+printf "ERROR_TEST result : $FLAG\n"
+
+#TLE, LEAKS, check ROOT
